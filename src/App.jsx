35a4ff3497982475168462a757edfee70572008e
MattParkin14 <matthew@parkin.co.nz>
2024-12-26 23:47:31 +1300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NameplateOrderForm from './components/NameplateOrderForm'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 flex items-center justify-center p-4">
+      <NameplateOrderForm />
+    </div>
+  )
+}
+
+export default App
