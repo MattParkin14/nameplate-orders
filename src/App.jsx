4c8f052676aa86db2b008dcaafe4b4b188d64e8e
MattParkin14 <matthew@parkin.co.nz>
2024-12-27 01:10:50 +1300

    backgroud

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import NameplateOrderForm from './components/NameplateOrderForm'
-//import BaseMaterialSelector from './components/BaseMaterialSelector';
+import BaseMaterialSelector from './components/BaseMaterialSelector';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 flex items-center justify-center p-4">
-      <NameplateOrderForm />
+    <div className="min-h-screen bg-gradient-to-br from-yellow-100 to-blue-200 flex justify-center items-center">
+      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6 mx-4 sm:mx-60">
+        <NameplateOrderForm />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
